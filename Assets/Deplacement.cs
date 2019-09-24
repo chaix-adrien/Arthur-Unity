f0083858b59483a210ddae32f4f6e4279f866231
Adrien Chaix <chaix.adrien@laposte.net>
2019-09-24 20:17:21 +0200

    Cours 2

---
+++ Assets/Deplacement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour
+{
+  // Start is called before the first frame update
+
+  private Rigidbody rb;
+
+  public int vitesse;
+  public int vie;
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  void checkKey()
+  {
+    if (Input.GetKeyDown("space"))
+    {
+      rb.velocity = rb.velocity + new Vector3(0, 1, 0) * vitesse;
+    }
+    if (Input.GetKey("q"))
+    {
+      rb.velocity = rb.velocity + new Vector3(-1, 0, 0) * vitesse * Time.deltaTime;
+    }
+    if (Input.GetKey("d"))
+    {
+      rb.velocity = rb.velocity + new Vector3(1, 0, 0) * vitesse * Time.deltaTime;
+    }
+    if (Input.GetKey("z"))
+    {
+      rb.velocity = rb.velocity + new Vector3(0, 0, 1) * vitesse * Time.deltaTime;
+    }
+    if (Input.GetKey("s"))
+    {
+      Debug.Log("le code passe bien ici");
+      rb.velocity = rb.velocity + new Vector3(0, 0, -1) * vitesse * Time.deltaTime;
+    }
+  }
+
+
+  void checkPosition()
+  {
+    if (transform.position.y < -1)
+    {
+      this.vie = this.vie - 1;
+      transform.position = new Vector3(0, 2, 0);
+    }
+  }
+
+  void Update()
+  {
+    this.checkKey();
+    this.checkPosition();
+
+  }
+}
